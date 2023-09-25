6f143710f5b4913343575308dcaf178d4931cd62
Baokhue312 <106755789+Baokhue312@users.noreply.github.com>
2023-09-25 22:37:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/daobaokhue_login_btdtdd/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.daobaokhue_login_btdtdd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
